67442468b2baa115a6103b63a9df1ff05acbebfa
Gerardo Miranda <gerardodev25@gmail.com>
2021-11-12 16:34:28 -0400

    1- creating the app

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Hello } from "./Hello";
-import { Info } from "./Info";
+import Task from "./Task";
 
-export const App = () => (
+const tasks = [
+  { _id: 1, text: "First Task" },
+  { _id: 2, text: "Second Task" },
+  { _id: 3, text: "Third Task" },
+];
+
+const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
+
+    <ul>
+      {tasks.map((task) => (
+        <Task key={task._id} task={task} />
+      ))}
+    </ul>
   </div>
 );
+export default App;
